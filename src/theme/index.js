296434aac489ef43aab0a2abae9a1c837df5d1a7
marccorpus <marccorpus.dev@gmail.com>
2022-05-01 23:40:56 +0800

    Dummy data, Theme

---
+++ src/theme/index.js
@@ -0,0 +1,50 @@
+const theme = {
+  colors: {
+    primary: '#007AFF',
+    white: '#FAFAFA',
+    lightGray: '#EBEBEB',
+    gray: '#CACACA',
+    black: '#000000',
+  },
+  spacing: {
+    xs: 4,
+    sm: 8,
+    m: 16,
+    l: 32,
+    xl: 40,
+  },
+  textVariants: {
+    h1: {
+      fontFamily: 'Nunito-Bold',
+      fontSize: 24,
+    },
+    h2: {
+      fontFamily: 'Nunito-Bold',
+      fontSize: 18,
+    },
+    body1: {
+      fontFamily: 'Nunito-Regular',
+      fontSize: 18,
+    },
+    body2: {
+      fontFamily: 'Nunito-Regular',
+      fontSize: 16,
+    },
+    body3: {
+      fontFamily: 'Nunito-Regular',
+      fontSize: 14,
+    },
+  },
+  borderRadius: {
+    xs: 1,
+    m: 10,
+  },
+  imageHeight: {
+    xs: 40,
+    s: 100,
+    m: 112,
+    l: 123,
+  },
+};
+
+export default theme;
